1cfd16034b13f3f20df017578e88ac879899da2f
m-jim-d <jim.miller.tc@gmail.com>
2026-02-20 11:51:15 -0600

    Updated documentation.

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# bin-method-calcs" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/m-jim-d/bin-method-calcs.git
-git push -u origin main
